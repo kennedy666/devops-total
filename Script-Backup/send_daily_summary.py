66f9c9c7b8843f3ced906ca8061026bf34607456
Mario Villa <Mario@gmail.com>
2025-04-04 12:33:08 +0200

    Auto-update repository

--- Script-Backup/send_daily_summary.py
+++ Script-Backup/send_daily_summary.py
@@ -1,26 +1,30 @@
 import os
 import smtplib
 from email.message import EmailMessage
-import sys
 
-from_email = os.getenv('MAIL_ADDRESS')
-password = os.getenv('EMAIL_PASSWORD')
-to_email = os.getenv('TO_EMAIL')
+# Variables de entorno
+from_email = os.environ.get('MAIL_ADDRESS')
+password = os.environ.get('EMAIL_PASSWORD')
+to_email = os.environ.get('TO_EMAIL')
 
+# Crear el mensaje
 msg = EmailMessage()
-msg['Subject'] = sys.argv[1]
+msg['Subject'] = "Resumen diario"
 msg['From'] = from_email
 msg['To'] = to_email
+msg.set_content("Este es el resumen de los backups del día de hoy, todo correcto.")
 
-# Leer resumen diario
-with open(sys.argv[2], 'r') as f:
-    body = f.read()
-msg.set_content(body)
+# Adjuntar backup.log
+log_path = "/app/backup.log"
+with open(log_path, 'rb') as f:
+    log_content = f.read()
+    msg.add_attachment(log_content, maintype='text', subtype='plain', filename='backup.log')
 
 try:
     with smtplib.SMTP('smtp.gmail.com', 587) as server:
         server.starttls()
         server.login(from_email, password)
         server.send_message(msg)
+        print("✅ Correo de resumen diario enviado correctamente.")
 except Exception as e:
-    print(f"Error al enviar el resumen diario: {e}")
+    print("❌ Error al enviar el correo diario:", e)
